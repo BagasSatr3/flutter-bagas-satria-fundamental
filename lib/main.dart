e4b403706b45f87369d6b69677b2cc11310e13d9
Bagas_Satria <bagassatjin23@gmail.com>
2023-04-05 17:10:38 +0000

    version 3.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,143 +1,136 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  static const String _title = 'Calculator';
-
   @override
   Widget build(BuildContext context) {
+    final appTitle = 'Calculator';
+
     return MaterialApp(
-      title: _title,
+      title: appTitle,
       home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatefulWidget(),
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: Calculator(),
       ),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({super.key});
-
+class Calculator extends StatefulWidget {
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  _CalculatorState createState() => _CalculatorState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
+class _CalculatorState extends State<Calculator> {
+  TextEditingController num1controller = new TextEditingController();
+  TextEditingController num2controller = new TextEditingController();
+  String result = "0";
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
+    return Container(
+      padding: EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('Result: 0',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold, fontSize: 30, height: 2)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-              decoration: const InputDecoration(
-                hintText: 'Enter First Number',
-              ),
-              validator: (String? value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
+          Text('Result : $result',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 30, height: 2)),
+          TextField(
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+            controller: num1controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'First Number',
+              hintText: 'Enter First Number',
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-              decoration: const InputDecoration(
-                hintText: 'Enter Second Number',
-              ),
-              validator: (String? value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+            controller: num2controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Second Number',
+              hintText: 'Enter Second Number',
             ),
+          ),
+          SizedBox(
+            height: 20,
           ),
           Center(
-            child: Padding(
+            child: Container(
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
+                child: Text("Add"),
                 onPressed: () {
-                  // Validate will return true if the form is valid, or false if
-                  // the form is invalid.
-                  if (_formKey.currentState!.validate()) {
-                    // Process data.
-                  }
+                  setState(() {
+                    int sum = int.parse(num1controller.text) +
+                        int.parse(num2controller.text);
+                    result = sum.toString();
+                  });
                 },
-                child: const Text('Add'),
               ),
             ),
           ),
           Center(
-            child: Padding(
+            child: Container(
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
+                child: Text("Subtract"),
                 onPressed: () {
-                  // Validate will return true if the form is valid, or false if
-                  // the form is invalid.
-                  if (_formKey.currentState!.validate()) {
-                    // Process data.
-                  }
+                  setState(() {
+                    int sum = int.parse(num1controller.text) -
+                        int.parse(num2controller.text);
+                    result = sum.toString();
+                  });
                 },
-                child: const Text('Substract'),
               ),
             ),
           ),
           Center(
-            child: Padding(
+            child: Container(
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
+                child: Text("Multipy"),
                 onPressed: () {
-                  // Validate will return true if the form is valid, or false if
-                  // the form is invalid.
-                  if (_formKey.currentState!.validate()) {
-                    // Process data.
-                  }
+                  setState(() {
+                    int sum = int.parse(num1controller.text) *
+                        int.parse(num2controller.text);
+                    result = sum.toString();
+                  });
                 },
-                child: const Text('Multipy'),
               ),
             ),
           ),
           Center(
-            child: Padding(
+            child: Container(
               padding: const EdgeInsets.all(8.0),
               child: ElevatedButton(
+                child: Text("Divide"),
                 onPressed: () {
-                  // Validate will return true if the form is valid, or false if
-                  // the form is invalid.
-                  if (_formKey.currentState!.validate()) {
-                    // Process data.
-                  }
+                  setState(() {
+                    int sum = int.parse(num1controller.text) ~/
+                        int.parse(num2controller.text);
+                    result = sum.toString();
+                  });
                 },
-                child: const Text('Divide'),
               ),
             ),
+          ),
+          SizedBox(
+            height: 20,
           ),
         ],
       ),
